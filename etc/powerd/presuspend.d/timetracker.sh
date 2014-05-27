68963e97c4200396c2a542e771c5aae0f6f2d988
Martin Abente Lahaye <tch@sugarlabs.org>
2014-05-27 19:14:46 -0400

    First unnumebered release
    Implements a sugar service that listens for power management
    events, in order to tell activities they are not longer active
    when suspend events occur.
    
    Adds powerd hooks for communicating these events to the sugar
    service.
    
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

---
+++ etc/powerd/presuspend.d/timetracker.sh
@@ -0,0 +1,1 @@
+dbus-send --system /org/sugarlabs/TimeTracker org.sugarlabs.TimeTracker.Suspend
---
+++ extensions/webservice/timetracker/account.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2013 Martin Abente Lahaye. - tch@sugarlabs.org
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+import dbus
+import logging
+
+from jarabe.model import shell
+from jarabe.webservice import account
+
+
+class Account(account.Account):
+
+    DBUS_SUSPEND_SIGNAL = 'Suspend'
+    DBUS_RESUME_SIGNAL = 'Resume'
+    DBUS_TRACKER_IFACE = 'org.sugarlabs.TimeTracker'
+
+    def __init__(self):
+        logging.debug('timetracker __init__')
+        self._model = shell.get_model()
+        self._activity = None
+
+        bus = dbus.SystemBus()
+        bus.add_signal_receiver(self.__suspend_cb,
+                                self.DBUS_SUSPEND_SIGNAL,
+                                self.DBUS_TRACKER_IFACE)
+        bus.add_signal_receiver(self.__resume_cb,
+                                self.DBUS_RESUME_SIGNAL,
+                                self.DBUS_TRACKER_IFACE)
+
+    def __suspend_cb(self):
+        logging.debug('timetracker __suspend_cb')
+        self._activity = self._model.get_active_activity()
+        if self._activity is not None:
+            self._activity.set_active(False)
+
+    def __resume_cb(self):
+        logging.debug('timetracker __resume_cb')
+        if self._activity is not None:
+            self._activity.set_active(True)
+        self._activity = None
+
+    def get_token_state(self):
+        return self.STATE_VALID
+
+
+def get_account():
+    return Account()
